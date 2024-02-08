2c04b051e08370208cdc686ddf0a403200e757c4
NoumanChohan <chohannouman95@gmail.com>
2024-02-08 17:48:52 +0500

    feat:working on loader package error

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,3 +2,4 @@ export {default as ActiveButton} from './ActiveButton';
 export {default as AuthenticationTextField} from './AuthenticationTextField';
 export {default as ProfileEditTextField} from './ProfileEditTextField';
 export {default as CustomVerticalLinearStepper} from './VerticalLinearStepper';
+export {default as Loader} from './Loader';
