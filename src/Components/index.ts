d9fb9fc12393de95863084fbf9475dece0401731
Muhammad Ghufran Ali <57570139+ghufran2508@users.noreply.github.com>
2024-02-29 00:09:45 +0500

    fix: rollup+package update

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -3,3 +3,4 @@ export {default as AuthenticationTextField} from './AuthenticationTextField';
 export {default as ProfileEditTextField} from './ProfileEditTextField';
 export {default as CustomVerticalLinearStepper} from './VerticalLinearStepper';
 export {default as Loader} from './Loader';
+export {default as RSidebar} from './SideBar';
