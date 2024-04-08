51db3cdc248cdac96f2571470b6eeb731bfcba13
ghufran2508 <ghoofy.321@gmail.com>
2024-04-08 18:15:50 +0500

    feat: RichTextEditor mui-tiptap added

---
+++ src/stories/RichTextEditor.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import RichTextEditorTipTap from '../Components/RichTextEditor/richTextEditor';
+
+const meta: Meta = {
+    title: 'Text Fields/RichTextEditorTipTap',
+    component: RichTextEditorTipTap,
+
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+    argTypes: {
+        editable: { control: 'boolean' },
+        value: { control: 'text' },
+        setValue: { control: 'function' }
+    }
+}
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+    args: {
+    }
+};
